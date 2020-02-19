f39b67c5e72c2ecc170d26c3074efdf7a06fd103
DanBaisal <dan.baisal92@gmail.com>
2020-02-20 00:00:53 +0100

    Finished the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending'; // here a pending class will be returned and applied if nothing was entered
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
+
+// There are 2 possible solutions to conditionally showing/hiding the success message, I'll  use one of them
